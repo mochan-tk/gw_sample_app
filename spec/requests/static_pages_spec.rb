32ce47cd04a10c71f044df20f439769aeab24297
mochan-tk <takashi1.kawamoto@gmail.com>
2015-05-02 17:21:08 +0900

    テストが通るようにタイトルを修正

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,11 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
   describe "Help page" do
@@ -13,6 +18,11 @@ describe "StaticPages" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
@@ -22,7 +32,13 @@ describe "StaticPages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
   end
+
 end
 #describe "StaticPages" do
 #  describe "GET /static_pages" do
