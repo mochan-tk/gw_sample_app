054091434a61f7179d6878cce7b878c25510399c
mochan-tk <takashi1.kawamoto@gmail.com>
2015-05-02 16:43:29 +0900

    commit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,14 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
   end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
 end
 #describe "StaticPages" do
 #  describe "GET /static_pages" do
